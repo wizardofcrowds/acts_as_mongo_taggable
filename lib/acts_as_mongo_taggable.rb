4375a122809c174e487e876f65e7bb3a70d1a9ab
Koichi Hirano <koichihirano@macbook2008.local>
2010-10-28 17:08:32 -0700

    activesupport backward compatibility

--- lib/acts_as_mongo_taggable.rb
+++ lib/acts_as_mongo_taggable.rb
@@ -202,6 +202,11 @@ end
 %w{ models observers }.each do |dir|
   path = File.join(File.dirname(__FILE__), 'app', dir)
   $LOAD_PATH << path
-  ActiveSupport::Dependencies.autoload_paths << path
+  if ActiveSupport::Dependencies.respond_to?(:autoload_paths)
+    ActiveSupport::Dependencies.autoload_paths << path
+  else
+    ActiveSupport::Dependencies.load_paths << path
+    ActiveSupport::Dependencies.load_once_paths.delete(path)
+  end
 end
 
